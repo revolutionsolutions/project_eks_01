90af9a3b554040d7d295ef12213eb3191b5b95b1
Prasad NJ <prasadjayarathna@gmail.com>
2026-06-01 10:15:51 +0200

    update

--- EKS/argocd.tf
+++ EKS/argocd.tf
@@ -11,6 +11,18 @@ provider "helm" {
     }
   }
 }
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
+
 ######################################################################################
 # Data Source
 data "aws_ecrpublic_authorization_token" "token" {
