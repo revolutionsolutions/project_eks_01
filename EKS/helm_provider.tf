f9b914fa7b7b4648516b38849ac602314ef9c397
Prasad NJ <prasadjayarathna@gmail.com>
2026-06-02 07:11:27 +0200

    eks

--- EKS/helm_provider.tf
+++ EKS/helm_provider.tf
@@ -2,15 +2,15 @@
 #   name = module.eks.cluster_name
 # }
 
-# data "aws_eks_cluster_auth" "eks" {
-#   name = module.eks.cluster_name
-# }
+data "aws_eks_cluster_auth" "eks" {
+  name = module.eks.cluster_name
+}
 
 provider "helm" {
   kubernetes = {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = module.eks.cluster_name.eks.token
+    token                  = data.aws_eks_cluster_auth.eks.token
   }
   
 }
