bd5734d061ce4e969c33c36c18043b1ffd27f521
Prasad NJ <prasadjayarathna@gmail.com>
2026-06-03 12:29:25 +0200

    eks-update-8

--- EKS/aws_lb.tf
+++ EKS/aws_lb.tf
@@ -20,7 +20,7 @@ resource "aws_iam_role" "aws_lbc" {
 }
 
 resource "aws_iam_policy" "aws_lbc" {
-  policy = file("./iam/AWSLoadBalancerController.json")
+  policy = file("./IAM/AWSLoadBalancerController.json")
   name   = "AWSLoadBalancerController"
 }
 
