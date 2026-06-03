3e8b9193290780976b4de9339a285e2419ea5031
Prasad NJ <prasadjayarathna@gmail.com>
2026-06-03 14:10:36 +0200

    eks-update-12

--- EKS/arogocd.tf
+++ EKS/arogocd.tf
@@ -13,6 +13,19 @@ resource "helm_release" "argocd" {
  depends_on = [aws_eks_node_group.eks_ngrp]
 }
 
+# Provides
+provider "helm" {
+  kubernetes = {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      command     = "aws"
+    }
+  }
+}
+
 #Install ingress for argocd service
 resource "kubernetes_ingress_v1" "argocd" {
   metadata {
