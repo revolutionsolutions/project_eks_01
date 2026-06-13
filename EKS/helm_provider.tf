6a105a099c1231ab726711c54f66392fc2420209
Prasad NJ <prasadjayarathna@gmail.com>
2026-06-13 09:34:41 +0200

    update cluster ca

--- EKS/helm_provider.tf
+++ EKS/helm_provider.tf
@@ -9,7 +9,8 @@ data "aws_eks_cluster_auth" "eks" {
 provider "helm" {
   kubernetes = {
     host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = module.eks.cluster_endpoint
+    #cluster_ca_certificate = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.eks.token
   }
   
